75e40e532c048c495eeded7eb71e097d8c7703c8
conmui <175262420+conmui@users.noreply.github.com>
2025-02-02 22:50:50 -0500

    Test Case 3
    'Test Case 3: Login User with incorrect email and password
    1. Launch browser
    2. Navigate to url 'http://automationexercise.com'
    3. Verify that home page is visible successfully
    4. Click on 'Signup / Login' button
    5. Verify 'Login to your account' is visible
    6. Enter incorrect email address and password
    7. Click 'login' button
    8. Verify error 'Your email or password is incorrect!' is visible'

---
+++ src/main/java/org/example/TestCase3.java
@@ -0,0 +1,49 @@
+package org.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TestCase3 extends BaseTest {
+//Test Case 3: Login User with incorrect email and password
+    @Test
+    public void loginNegativeTest() {
+        String emailIncorrect = "abcc@email.com";
+        String passwordIncorrect = "itsalwayssunny0";
+        String email = "abck@email.com";
+        String password = "itsalwayssunny";
+
+//1. Launch browser
+//2. Navigate to url 'http://automationexercise.com'
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("http://automationexercise.com");
+
+//3. Verify that home page is visible successfully
+        verifyHomepageVisible();
+
+//4. Click on 'Signup / Login' button
+        clickBtn(By.linkText("Signup / Login"));
+
+//5. Verify 'Login to your account' is visible
+        verifyHeaderVisible(By.cssSelector(".login-form h2"), "Login to your account");
+
+//6. Enter incorrect email address and password
+//        fillInput(By.cssSelector("[data-qa='login-email']"), emailIncorrect);
+        fillInput(By.cssSelector("[data-qa='login-password']"), passwordIncorrect);
+//6a. Enter incorrect email address or password
+        fillInput(By.cssSelector("[data-qa='login-email']"), email);
+//        fillInput(By.cssSelector("[data-qa='login-password']"), password);
+
+//7. Click 'login' button
+        clickBtn(By.cssSelector("[data-qa='login-button']"));
+
+//8. Verify error 'Your email or password is incorrect!' is visible
+        WebElement errorElem = driver.findElement(By.cssSelector(".login-form p"));
+        boolean isErrorVisible = errorElem.isDisplayed();
+        String errorText = errorElem.getText();
+        assertTrue(isErrorVisible);
+        assertEquals("Your email or password is incorrect!", errorText);
+    }
+}
