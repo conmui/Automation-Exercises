f189cb1103fe52783e490896d0ca6776eb9e5333
conmui <175262420+conmui@users.noreply.github.com>
2025-02-02 23:22:47 -0500

    Test Case 4
    'Test Case 4: Logout User
    1. Launch browser
    2. Navigate to url 'http://automationexercise.com'
    3. Verify that home page is visible successfully
    4. Click on 'Signup / Login' button
    5. Verify 'Login to your account' is visible
    6. Enter correct email address and password
    7. Click 'login' button
    8. Verify that 'Logged in as username' is visible
    9. Click 'Logout' button
    10. Verify that user is navigated to login page'

---
+++ src/main/java/org/example/TestCase4.java
@@ -0,0 +1,58 @@
+package org.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class TestCase4 extends BaseTest {
+//Test Case 4: Logout User
+    @Test
+    public void logoutTest() {
+        String username = "abck";
+        String email = "abck@email.com";
+        String password = "itsalwayssunny";
+        String loggedInStatus = "Logged in as " + username;
+
+//1. Launch browser
+//2. Navigate to url 'http://automationexercise.com'
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("http://automationexercise.com");
+
+//3. Verify that home page is visible successfully
+        verifyHomepageVisible();
+
+//4. Click on 'Signup / Login' button
+        clickBtn(By.linkText("Signup / Login"));
+
+//5. Verify 'Login to your account' is visible
+        verifyHeaderVisible(By.cssSelector(".login-form h2"), "Login to your account");
+
+//6. Enter correct email address and password
+        fillInput(By.cssSelector("[data-qa='login-email']"), email);
+        fillInput(By.cssSelector("[data-qa='login-password']"), password);
+
+//7. Click 'login' button
+        clickBtn(By.cssSelector("[data-qa='login-button']"));
+
+//8. Verify that 'Logged in as username' is visible
+        verifyHeaderVisible(By.cssSelector(".navbar-nav li:last-child"), loggedInStatus);
+
+//9. Click 'Logout' button
+        clickBtn(By.linkText("Logout"));
+
+//10. Verify that user is navigated to login page
+        verifyLoginPageVisible(loggedInStatus);
+    }
+
+    private void verifyLoginPageVisible(String loggedInStatus) {
+        String title = driver.getTitle();
+        assertEquals("Automation Exercise - Signup / Login", title);
+
+        verifyHeaderVisible(By.cssSelector(".login-form h2"), "Login to your account");
+
+        //check "Logged in as <username>" does not show indicating user has logged out
+        String lastNavbarItem = driver.findElement(By.cssSelector(".navbar-nav li:last-child")).getText();
+        assertNotEquals(loggedInStatus, lastNavbarItem);
+    }
+}
