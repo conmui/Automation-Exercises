261d58287f48eedfec961bf4a3e8457f288e5640
conmui <175262420+conmui@users.noreply.github.com>
2025-02-03 20:43:35 -0500

    Test Case 6
    'Test Case 6: Contact Us Form
    1. Launch browser
    2. Navigate to url 'http://automationexercise.com'
    3. Verify that home page is visible successfully
    4. Click on 'Contact Us' button
    5. Verify 'GET IN TOUCH' is visible
    6. Enter name, email, subject and message
    7. Upload file
    8. Click 'Submit' button
    9. Click OK button
    10. Verify success message 'Success! Your details have been submitted successfully.' is visible
    11. Click 'Home' button and verify that landed to home page successfully'

---
+++ src/main/java/org/example/TestCase6.java
@@ -0,0 +1,55 @@
+package org.example;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Alert;
+
+public class TestCase6 extends BaseTest {
+    @Test
+//    Test Case 6: Contact Us Form
+    public void contactUsFormTest() {
+        String subject = "Lorem Ipsum";
+        String message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        String uploadFilePath = "";
+
+//1. Launch browser
+//2. Navigate to url 'http://automationexercise.com'
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("http://automationexercise.com");
+
+//3. Verify that home page is visible successfully
+        verifyHomepageVisible();
+
+//4. Click on 'Contact Us' button
+        clickBtn(By.linkText("Contact us"));
+
+//5. Verify 'GET IN TOUCH' is visible
+        verifyHeaderVisible(By.cssSelector(".contact-form .title"), "GET IN TOUCH");
+
+//6. Enter name, email, subject and message
+        fillInput(By.cssSelector("[data-qa='name']"), testUser.getFirstName() + " " + testUser.getLastName());
+        fillInput(By.cssSelector("[data-qa='email']"), testUser.getEmail());
+        fillInput(By.cssSelector("[data-qa='subject']"), subject);
+        fillInput(By.cssSelector("[data-qa='message']"), message);
+
+//7. Upload file
+        WebElement fileInput = driver.findElement(By.name("upload_file"));
+//        fileInput.sendKeys(uploadFilePath);
+
+//8. Click 'Submit' button
+        clickBtn(By.cssSelector("[data-qa='submit-button']"));
+
+//9. Click OK button
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+//10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+        verifyNotificationMessage(By.cssSelector(".contact-form .alert-success"), "Success! Your details have been submitted successfully.");
+
+//11. Click 'Home' button and verify that landed to home page successfully
+        clickBtn(By.linkText("Home"));
+
+        verifyHomepageVisible();
+    }
+}
